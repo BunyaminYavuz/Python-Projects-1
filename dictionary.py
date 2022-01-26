4c95973335fe20c4c9135ed6e2024b53639c8adc
Byavuz07 <91992194+Byavuz07@users.noreply.github.com>
2022-01-27 00:10:57 +0300

    Dictionary

---
+++ dictionary.py
@@ -0,0 +1,35 @@
+# Dictionary
+
+words = dict()
+
+# 1st word
+english_word = input("Write that word you learned :")
+turkish_word = input("Write mean in turkish :")
+
+words[english_word] = turkish_word
+
+# 2sd word
+english_word = input("Write that word you learned :")
+turkish_word = input("Write mean in turkish :")
+
+words[english_word] = turkish_word
+
+# 3rd word
+english_word = input("Write that word you learned :")
+turkish_word = input("Write mean in turkish :")
+
+words[english_word] = turkish_word
+
+# 4th word
+english_word = input("Write that word you learned :")
+turkish_word = input("Write mean in turkish :")
+
+words[english_word] = turkish_word
+
+# 5th word
+english_word = input("Write that word you learned :")
+turkish_word = input("Write mean in turkish :")
+
+words[english_word] = turkish_word
+
+print(words)
